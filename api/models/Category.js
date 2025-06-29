07ddd1ac8294bdb6d655cae04390915ab59a140b
gms1264 <gms962738@gmail.com>
2025-06-29 14:03:42 +0530

    Update Category.js

--- api/models/Category.js
+++ api/models/Category.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
 });
-
+ 
 const Category = mongoose.model('Category', categorySchema);
 module.exports = Category;
